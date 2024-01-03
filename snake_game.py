6ef5b560533957041fb2a3b53dbac2a544029287
EkEkwonna <kennyekwonna@gmail.com>
2024-01-03 19:16:25 +0000

    Up to Stage 3

---
+++ snake_game.py
@@ -0,0 +1,205 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np 
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+#font = pygame.font.SysFont('arial', 25)
+
+
+"""
+Requirements: 
+* Need to create a reset function so we can reset the games 
+* Need to implement a reward for the agent to mark a score 
+* Need to change play function so that play(action) -> DIRECTION
+* Need to keep track of iterations (game_iteration)
+* Need a change in the if is_collision function 
+* 
+"""
+
+
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+    
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+BLOCK_SIZE = 20
+SPEED = 20
+
+class SnakeGameAI:
+    
+    def __init__(self, w=640, h=480):
+        
+        "w: width , h: height"
+        
+        self.w = w
+        self.h = h
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+        self.reset()
+        
+        # init game state
+        self.direction = Direction.RIGHT
+        
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+
+    """
+    Created a reset() helper function 
+    taking the same constructors as the Snakegame class
+    """
+    def reset(self):
+        # init game state
+        self.direction = Direction.RIGHT
+        
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
+        self._place_food()
+
+
+        
+    def _place_food(self):
+        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
+        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
+        
+    def play_step(self,action):
+        """
+        # 1 : Collects user input (key that's pressed)
+        # 2 : Calculate the move based on key updates the head
+        # 3 : Check if game is over if not 
+        # 4 : Replace the food 
+        """
+
+        self.frame_iteration += 1 
+        # 1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        # 2. move
+        self._move(action) # update the head
+        self.snake.insert(0, self.head)
+        
+        # 3. check if game over
+        reward = 0
+        game_over = False
+        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
+            game_over = True
+            reward = -10
+            return reward , game_over, self.score
+            
+        # 4. place new food or just move
+        if self.head == self.food:
+            self.score += 1
+            reward += 10
+            self._place_food()
+        else:
+            self.snake.pop()
+        
+        # 5. update ui and clock
+        self._update_ui()
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        return reward , game_over, self.score
+    
+
+    def _is_collision(self , pt = None):
+        
+        """
+        pt = Point arguement confirm the point at where the head of the snake is 
+        w = width  = x-axis of the game rectangle 
+        h = height = y-axis of the game rectange 
+
+        THEORY: 
+        SNAKE = bunch of coordinates 
+        every point of rectangle will have a (w,h) coordinate
+        """
+
+        if pt is None: 
+            pt = self.head
+        # hits boundary
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+        # hits itself
+        if pt in self.snake[1:]:
+            return True
+        
+        return False
+        
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+            
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
+        
+    def _move(self, action):
+        # [straight, right , left ]
+        # idx = index
+        clock_wise = [Direction.RIGHT, Direction.DOWN , Direction.LEFT , Direction.UP]
+        idx = clock_wise.index(self.direction)
+        
+        if np.array_equal(action, [1,0,0]):
+            new_dir = clock_wise[idx] # No change
+
+        elif np.array_equal(action,[0,1,0]):
+            next_idx = (idx + 1 ) % 4
+            new_dir = clock_wise[next_idx ] #right turn r -> d -> l -> u 
+        
+        else np.array_equal(action,[0,0,1]):
+            next_idx = (idx - 1) % 4
+            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+
+
+        x = self.head.x
+        y = self.head.y
+        if direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif direction == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif direction == Direction.UP:
+            y -= BLOCK_SIZE
+            
+        self.head = Point(x, y) 
+
+        "^^ This calculates the new position of the new head"
+            
